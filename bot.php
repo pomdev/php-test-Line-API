1eaa304fc215c8ad9418c8f69edbe394137a1781
ek <ek@wealth65.com>
2017-07-05 04:35:43 +0700

    Update Security

--- bot.php
+++ bot.php
@@ -1,4 +1,10 @@
 <?php
+    
+$proxy = 'http://fixie:JBHYLVfOyu7ZUvg@velodrome.usefixie.com:80';
+$proxyauth = 'ek@wealth65.com:P@ssw0rd@2017';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
 $access_token = 'mOO1A40zvrJda8pQtdHRg3t2Ns/ui0axNP1nhiX3z5XWDoCHSP9x99u/XZWG2BfsX9ex4S0P060Hk9RPNXPgc72Sr6VxN+mO6sUhtSf5iSekhT72RI0trKd/HaxJY3dRc90ZUUrNuyG7tkhiqpyvEwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
