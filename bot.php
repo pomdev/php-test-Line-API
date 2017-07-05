33781bfdac5ade46ac4b7aa4759fbb1c4085b3f9
ek <ek@wealth65.com>
2017-07-05 15:05:26 +0700

    changed

--- bot.php
+++ bot.php
@@ -43,14 +43,14 @@ if (!is_null($events['events'])) {
 			echo $result . "\r\n";
 
 
-        }else{
+        } else {
     		    // Get replyToken
 			    $replyToken = $event['replyToken'];
 
 			    // Build message to reply back
 			    $messages = [
 				    'type' => 'text',
-				    'text' => "555"
+				    'text' = "555";
 			    ];
 
             	// Make a POST Request to Messaging API to reply to sender
