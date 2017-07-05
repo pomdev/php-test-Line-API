35a25e144c69d8a0cc30397887ab2a28a5775ad1
ek <ek@wealth65.com>
2017-07-05 11:11:32 +0700

    Switch Lines

--- verify.php
+++ verify.php
@@ -1,9 +1,7 @@
 <?php
 $access_token = 'mOO1A40zvrJda8pQtdHRg3t2Ns/ui0axNP1nhiX3z5XWDoCHSP9x99u/XZWG2BfsX9ex4S0P060Hk9RPNXPgc72Sr6VxN+mO6sUhtSf5iSekhT72RI0trKd/HaxJY3dRc90ZUUrNuyG7tkhiqpyvEwdB04t89/1O/w1cDnyilFU=';
-
-$url = 'https://api.line.me/v1/oauth/verify';
-
 $headers = array('Authorization: Bearer ' . $access_token);
+$url = 'https://api.line.me/v1/oauth/verify';
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
